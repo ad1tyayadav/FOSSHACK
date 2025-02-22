924d788a9a47581ebd86145acd1abbb25e67521e
ad1tyayadav <adityaproworks@gmail.com>
2025-02-22 18:37:11 +0530

    Fixed npm packages

--- uichatlib/vite.config.js
+++ uichatlib/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   build: {
@@ -18,5 +19,8 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 });
